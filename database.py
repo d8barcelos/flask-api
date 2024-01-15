3fc5ab3c879d0b2178b540505e22176c427a80b1
d8barcelos <d8barcelos@gmail.com>
2024-01-15 15:41:38 -0300

    finish crud functions

---
+++ database.py
@@ -0,0 +1,107 @@
+Carros = [
+    {
+      "id": 1,
+      "marca": "Toyota",
+      "modelo": "Corolla",
+      "ano": 2022,
+      "cor": "prata"
+    },
+    {
+      "id": 2,
+      "marca": "Honda",
+      "modelo": "Civic",
+      "ano": 2023,
+      "cor": "preto"
+    },
+    {
+      "id": 3,
+      "marca": "Ford",
+      "modelo": "Mustang",
+      "ano": 2021,
+      "cor": "azul"
+    },
+    {
+      "id": 4,
+      "marca": "Chevrolet",
+      "modelo": "Camaro",
+      "ano": 2022,
+      "cor": "vermelho"
+    },
+    {
+      "id": 5,
+      "marca": "Volkswagen",
+      "modelo": "Golf",
+      "ano": 2023,
+      "cor": "branco"
+    },
+    {
+      "id": 6,
+      "marca": "BMW",
+      "modelo": "X5",
+      "ano": 2022,
+      "cor": "preto"
+    },
+    {
+      "id": 7,
+      "marca": "Mercedes-Benz",
+      "modelo": "C-Class",
+      "ano": 2021,
+      "cor": "prata"
+    },
+    {
+      "id": 8,
+      "marca": "Audi",
+      "modelo": "A4",
+      "ano": 2023,
+      "cor": "azul"
+    },
+    {
+      "id": 9,
+      "marca": "Nissan",
+      "modelo": "Altima",
+      "ano": 2022,
+      "cor": "vermelho"
+    },
+    {
+      "id": 10,
+      "marca": "Hyundai",
+      "modelo": "Elantra",
+      "ano": 2021,
+      "cor": "branco"
+    },
+    {
+      "id": 11,
+      "marca": "Kia",
+      "modelo": "Optima",
+      "ano": 2023,
+      "cor": "prata"
+    },
+    {
+      "id": 12,
+      "marca": "Mazda",
+      "modelo": "Mazda3",
+      "ano": 2022,
+      "cor": "preto"
+    },
+    {
+      "id": 13,
+      "marca": "Subaru",
+      "modelo": "Outback",
+      "ano": 2021,
+      "cor": "verde"
+    },
+    {
+      "id": 14,
+      "marca": "Jaguar",
+      "modelo": "F-PACE",
+      "ano": 2023,
+      "cor": "azul"
+    },
+    {
+      "id": 15,
+      "marca": "Lexus",
+      "modelo": "RX",
+      "ano": 2022,
+      "cor": "prata"
+    }
+]
--- main.py
+++ main.py
@@ -1,6 +1,64 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
+from database import Carros
 
 app = Flask(__name__)
 
-app.run
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    return make_response (
+        jsonify (
+            message='Lista de carros',
+            data=Carros
+        )
+    )
+
+@app.route('/carros', methods=['POST'])
+def create_car():
+    car = request.json
+    Carros.append(car)
+    return make_response(
+        jsonify(
+            message='Carro cadastrado com sucesso!',
+            car=car
+        )
+    )
+
+@app.route('/carros/<int:car_id>', methods=['DELETE'])
+def delete_car(car_id):
+    if car_id < len(Carros):
+        deleted_car = Carros.pop(car_id)
+        return make_response(
+            jsonify(
+                message='Carro removido com sucesso!',
+                deleted_car=deleted_car
+            )
+        )
+    else:
+        return make_response(
+            jsonify(
+                message=f'Carro com ID {car_id} não encontrado!',
+            ),
+            404
+        )
+
+@app.route('/carros/<int:car_id>', methods=['PUT'])
+def update_car(car_id):
+    if car_id < len(Carros):
+        updated_car_data = request.json
+        Carros[car_id].update(updated_car_data)
+        return make_response(
+            jsonify(
+                message='Carro atualizado com sucesso!',
+                updated_car=Carros[car_id]
+            )
+        )
+    else:
+        return make_response(
+            jsonify(
+                message=f'Carro com ID {car_id} não encontrado!',
+            ),
+            404
+        ) 
+
+app.run()
 
